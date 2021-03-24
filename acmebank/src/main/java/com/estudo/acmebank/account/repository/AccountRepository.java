00c7d381dfbf5deb250a11d9bdd00e17a4325594
Diego Patricio <50188362+diegojpatricio@users.noreply.github.com>
2021-03-24 14:50:13 -0300

    Delete acmebank directory

--- acmebank/src/main/java/com/estudo/acmebank/account/repository/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.estudo.acmebank.account.repository;
-
-import com.estudo.acmebank.account.model.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-}
